2cfa9dd371eefe0d70ce7460ed34365988bd4c9e
dachamyra <dachamyrah@gmail.com>
2022-09-09 19:27:31 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - prints all single digit numbers of base ten from zero
+*Description: using the main function
+*this program prints all single digit numbers of base ten from zero
+*Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+	printf("%i", i);
+}
+printif("\n");
+return (0);
+}
