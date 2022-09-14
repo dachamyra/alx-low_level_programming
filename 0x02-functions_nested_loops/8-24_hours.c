fddbc8be8a7cd6cf1f0cb78e0dc9c48c07b2ae13
dachamyra <dachamyrah@gmail.com>
2022-09-14 16:11:56 -0700

    Program that prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,11 +18,10 @@ void jack_bauer(void)
 	{
 		minutes_remainder = minutes % 10;
 		hours_remainder = hours % 10;
-		_putchar(hours / 10 + '0' );
-		
+		_putchar(hours / 10 + '0');
 _putchar(hours_remainder + '0');
-		_putchar( ':' );
-		_putchar(minutes / 10 + '0' );
+		_putchar(':');
+		_putchar(minutes / 10 + '0');
 
 _putchar(minutes_remainder + '0');
 		minutes++;
