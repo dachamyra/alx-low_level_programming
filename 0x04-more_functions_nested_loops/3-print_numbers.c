634c907bd32b7b8c8de5609a56a41f78ef7d6cd8
dachamyra <dachamyrah@gmail.com>
2022-09-15 15:20:02 -0700

    Program that prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 58; n++)
+	{
+		_putchar(n);
+	}
+
+	_putchar('\n');
+}
