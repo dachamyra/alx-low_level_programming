dbb6afe5599ab0ff58d2fde011fa1a141c9416b1
dachamyra <dachamyrah@gmail.com>
2022-09-22 13:50:06 -0700

    0.strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @src: the string to be appended
+ * @dest: A pointer to the string to be concatenated
+ *
+ * Return: A pointer to the destination string @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
