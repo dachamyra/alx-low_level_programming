1240333fbb0902d06ac3254f9886e7b7f49eaa80
dachamyra <dachamyrah@gmail.com>
2022-09-26 16:04:03 -0700

    Hot fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,6 @@
  * _strspn - function that gets the length of a prefix substring
  * @s: string to check
  * @accept: string to check against
- * 
  * Return: the number of bytes of s consisting of only bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
