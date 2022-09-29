d97fff1500689caaf9105de43b253a5135999ea0
dachamyra <dachamyrah@gmail.com>
2022-09-29 13:49:34 -0700

    Function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts_recursion - function that prints a string
@@ -9,14 +10,15 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
 
-		_putchar('\n');
+		putchar(*s);
+
+		_puts_recursion(s + 1);
 	}
 	else
 	{
-		_putchar(s[0]);
-		_puts_recursion(s + 1);
+		putchar('\n');
 	}
 }
