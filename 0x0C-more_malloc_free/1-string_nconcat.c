a47de2536f5d3e34815159f8d48d9b0ea4f74f76
Cassandra Dacha <cassie.dacha@gmail.com>
2022-10-07 01:33:35 +0300

    Function that concatenated two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *string_nconcat - function that concatenates two strings
+ * @s1: string to be appended
+ * @s2: string to append
+ * @n: memory to allocate
+ * Return: pointer containing s1 followed by n bytes of s2
+ * if function fails it should return NULL
+ * if n is greater or equal to the length of s2 use the entire s2 string
+ * if NULL is passed treat it as an empty string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, k;
+	char *s;
+
+	if (s1 == NULL)
+		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	if (j > n)
+		j = n;
+	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
+		return (NULL);
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
+	return (s);
+}
